3c122234319b152e51604ba7af67cdcc9c66697c
hisaaki <iida@dragon.t.u-tokyo.ac.jp>
2024-11-26 04:31:36 +0900

    [Beetle][Hand] modified for doubletag_detection flightnav and add camera on gazebo

--- robots/beetle/script/apriltag/apriltagposition_simulation.py
+++ robots/beetle/script/apriltag/apriltagposition_simulation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-import rospy
+import os, tf, math, rospy, time
+import numpy as np
+import xml.etree.ElementTree as ET
 from apriltag_ros.msg import AprilTagDetectionArray
 from std_msgs.msg import Float64, Bool
-from geometry_msgs.msg import Twist, PoseStamped
+from geometry_msgs.msg import Twist, PoseStamped, Pose, Point, Transform, Quaternion
 from aerial_robot_msgs.msg import FlightNav
-import numpy as np
-import tf
-import time
-
+from gazebo_msgs.srv import SpawnModel
 
 class AprilPIDController:
     def __init__(self):
@@ -60,6 +59,10 @@ class AprilPIDController:
         # Flight navigation message
         self.nav_msg = FlightNav()
 
+    def euler_to_quaternion(self, roll, pitch, yaw):
+        q = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+        return Quaternion(x = q[0], y = q[1], z = q[2], w = q[3])
+        
     def calculateError(self):
         self.err = self.targetPosition - self.currentDistance
         self.err_i += self.err
@@ -77,6 +80,53 @@ class AprilPIDController:
             (orientation.x, orientation.y, orientation.z, orientation.w)
         )
 
+    def spawnModel(self):
+        model_path = f'/home/iida/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/robots/beetle/models/sim/doubletag/doubletag_texture.sdf'
+
+        try:
+            with open(model_path, 'r') as model_file:
+                model_sdf = model_file.read()
+            tree = ET.parse(model_path)
+            root = tree.getroot()
+            pose_element = root.find('.//pose')
+        
+            if pose_element is not None:
+                pose_values = list(map(float, pose_element.text.split()))
+                x, y, z = pose_values[0:3]
+                roll, pitch, yaw = pose_values[3:6]
+    
+                pose = Point(x, y, z)
+                orientation = self.euler_to_quaternion(roll, pitch, yaw)
+                initial_pose = Pose(pose, orientation)
+
+            else:
+                print("\033[1;91m[Warn] No <pose> tag found in the model file.\033[0m")
+                return None, None, None
+
+            inertia_element = root.find('.//inertia')
+            if inertia_element is not None:
+                inertia = {}
+                inertia['m'] = float(inertia_element.find('./mass').text)
+                inertia['ixx'] = float(inertia_element.find('./ixx').text)
+                inertia['ixy'] = float(inertia_element.find('./ixy').text)
+                inertia['ixz'] = float(inertia_element.find('./ixz').text)
+                inertia['iyy'] = float(inertia_element.find('./iyy').text)
+                inertia['iyz'] = float(inertia_element.find('./iyz').text)
+                inertia['izz'] = float(inertia_element.find('./izz').text)
+
+            else:
+                print("\033[1;91m[Warn] No <inertia> tag found in the model file.\033[0m")
+                return None, None, None
+
+            rospy.wait_for_service('/gazebo/spawn_sdf_model')
+            spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+            spawn_sdf(model_name="doubletag", model_xml=model_sdf, robot_namespace="beetle", initial_pose=initial_pose, reference_frame="world")
+            rospy.loginfo("\033[1;32m[Msg] Model spawned successfully.\033[0m")
+
+        except rospy.ServiceException as e:
+            rospy.logerr("\033[1;91m[Warn] Spawn service call failed: %s\033[0m" % e)
+            return None, None, None
+        
     def callback1(self, data):
         if data.detections:
             detected_ids = [detection.id[0] for detection in data.detections]
@@ -111,6 +161,10 @@ class AprilPIDController:
             self.currentEuler = self.lastEuler
 
     def main(self):
+        rospy.sleep(1)
+
+        self.spawnModel()
+        
         while not rospy.is_shutdown():
 
             # Error calculating
@@ -141,7 +195,6 @@ class AprilPIDController:
             self.pub.publish(self.nav_msg)
 
             self.rate.sleep()
-
 
 if __name__ == "__main__":
     try:
